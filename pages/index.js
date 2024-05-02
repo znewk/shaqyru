1646c1c73763fed2429fe95b55c1d2d054b322b0
znewk <zane.css34@gmail.com>
2024-05-02 16:50:12 +0500

    asdasd

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,9 @@ const Index = () => {
                             </div>
 
                             <span style={{textAlign: 'center', display: 'block', }}>
-                            үйлену тойына арналған салтанатты ақ дастарханымыздың қадірлі қонағы болуға шақырамыз! 
+                            үйлену тойына арналған салтанатты ақ дастарханымыздың қадірлі қонағы болуға шақырамыз! <br/> <br/>
+                            той иелерІ:   <br/>
+                            <span className={styles.namesss}>Бахытжан - Айгуль</span>
                             </span>
                         </div>
                     </div>
@@ -100,9 +102,108 @@ const Index = () => {
                             сіздерге ыңғайлы болуы үшін төменде көрсетілген 2gis картаны пайдалануыңызды сұраймыз
                             </span>
                         </div>
-                    
+
                         <a href="https://2gis.kz/astana/geo/70000001055312155" className={styles.link}>2GIS</a>
                     </div>
+
+
+                    <div className={styles.secondBlock} style={{marginTop: 30}}>
+                        <div className={styles.header} style={{paddingBottom: 0}}>
+                            <span>———</span>
+                            <div style={{padding: '0 15px', boxSizing: 'border-box', textAlign: 'center'}}>
+                                <span>Той күнінің бағдарламасы:</span> <br/>
+                            </div>
+                            <span style={{display: 'flex', justifyContent: 'flex-end'}}>———</span>
+                        </div>
+
+
+                        <div className={styles.wrapper}>
+                            <div className={styles.row}>
+                                <Image src={'/IMG_0896.png'} height={100} width={100}/>
+
+                                <div className={styles.item}>
+                                    <span className={styles.time}>16:00</span> <br/>
+                                    <span style={{marginTop: '-10px'}}>Қонақтардың жиналуы</span>
+                                </div>
+                            </div>
+
+
+                            <div className={styles.row}>
+                                <div className={styles.item}>
+                                    <span className={styles.time}>16:30</span> <br/>
+                                    <span style={{marginTop: '-10px'}}>фотосессия, Фуршет</span>
+                                </div>
+
+                                <Image src={'/IMG_0898.png'} height={100} width={100}/>
+                            </div>
+
+                            <div className={styles.row}>
+                                <Image src={'/IMG_0901.png'} height={100} width={100}/>
+
+                                <div className={styles.item}>
+                                    <span className={styles.time}>17:00</span> <br/>
+                                    <span style={{marginTop: '-10px'}}>Тойдың басталуы</span>
+                                </div>
+                            </div>
+
+                            <div className={styles.row}>
+                                <div className={styles.item}>
+                                    <span className={styles.time}>19:00</span> <br/>
+                                    <span style={{marginTop: '-10px'}}>Қалыңдық пен күйеужігіт вальсі</span>
+                                </div>
+
+                                <Image src={'/IMG_0905.png'} height={100} width={100}/>
+                            </div>
+
+                            <div className={styles.row}>
+                                <Image src={'/IMG_0909.png'} height={100} width={100}/>
+
+                                <div className={styles.item}>
+                                    <span className={styles.time}>20:00</span> <br/>
+                                    <span style={{marginTop: '-10px'}}>Музыкалық би, шоу бағдарлама</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div style={{display: 'flex', justifyContent: 'center', paddingBottom: 50}}>
+                        <Image src={'/List.png'} width={200} height={200} style={{transform: 'rotate(75deg)', marginTop: '-50px'}}/>
+                    </div>
+
+                    <div className={styles.secondBlock} style={{marginTop: 30, paddingTop: 30}}>
+                        <span className={styles.namesss} style={{display: 'block', textAlign: 'center', fontSize: 64}}>Дресс код:</span>
+
+                        <div className={styles.wrapper2}>
+                            <div className={styles.wrapperItem}>
+                                <Image src={'/2.png'} height={100} width={120}/>
+                                <span>
+                                Ерлер қауымы: Классикалық костюм     
+                                </span>
+                            </div>
+                            <div className={styles.wrapperItem}>
+                                <Image src={'/1.png'} height={100} width={120}/>
+                                <span>
+                                Ерлер қауымы: Классикалық костюм     
+                                </span>
+                            </div>
+                        </div>
+
+
+                        <div className={styles.header}>
+                            <span>———</span>
+                            <div style={{padding: '0 15px', boxSizing: 'border-box', textAlign: 'center'}}>
+                                <span>Келіңіздер, тойымыздың қадірлі қонағы болыңыздар! </span> <br/>
+                            </div>
+                            <span style={{display: 'flex', justifyContent: 'flex-end'}}>———</span>
+                        </div>
+                        
+                    </div>
+
+                    <div style={{display: 'flex', justifyContent: 'center', paddingBottom: 50}}>
+                        <Image src={'/List.png'} width={200} height={200} style={{transform: 'rotate(75deg)', marginTop: '-50px'}}/>
+                    </div>
+
+                    <audio src="/Wedding Day.mp4" autoPlay loop />
                 </div>
             </div>
 
